cf60a6ba30840ac1b65c2676055a785921432d74
LinceIan <ianlince@gmail.com>
2022-11-01 12:02:28 -0300

    Bootstrap + Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField(max_lenght=180)
+
+    def __str__(self):
+        return self.nome
