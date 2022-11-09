00fb94ed038e557ea9792984a381932816e12205
Joao Victor Geraldini Matos <geraldinimatos@gmail.com>
2022-11-08 22:20:00 -0300

    frontend. Primeira versao

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from statistics import mode
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Usuario(models.Model):
-    nome = models.CharField(max_lenght=180)
+class Usuario(User):
+    nome = models.CharField(max_length= 180)
 
-    def __str__(self):
+    def str(self):
         return self.nome
